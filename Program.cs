0ec49af2830510a76dbfca6ab4a95fa6e0571948
Ki-van <kirill.ivanin00@yandex.ru>
2021-06-04 00:33:55 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LB_4_MMK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
